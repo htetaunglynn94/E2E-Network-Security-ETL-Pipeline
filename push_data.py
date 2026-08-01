402680e701850ab3a50db18bc8953ebc484aa69f
Htet Aung Lynn <htetaunglynn94@gmail.com>
2026-08-01 17:17:41 +0800

    Created ETL pipeline

--- push_data.py
+++ push_data.py
@@ -27,6 +27,35 @@ class NetworkDataExtract():
         try:
             data = pd.read_csv(file_path)
             data.reset_index(drop=True, inplace=True)
-            records = (data.T.to_json())
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+
+    def insert_data_to_mongodb(self, records, database, collection):
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+if __name__ == "__main__":
+    FILE_PATH = "network_data/phisingData.csv"
+    DATABASE = "HtetAungLynn"
+    COLLECTION = "NetworkData"
+
+    networkobj = NetworkDataExtract()
+    RECORDS = networkobj.csv_to_json_converter(file_path=FILE_PATH)
+    print(RECORDS)
+    no_of_records = networkobj.insert_data_to_mongodb(RECORDS, DATABASE, COLLECTION)
+    print(no_of_records)
